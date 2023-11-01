825743a1522eb1c13dc5a47769f96cfd754d2893
MomtajHossainMow <momtajhossain2001@gmail.com>
2023-11-01 17:42:35 +0600

    Changed Time Zone & Removed Price Field From Book

--- books/models.py
+++ books/models.py
@@ -1,16 +1,16 @@
 # books/models.py
 from django.db import models
 
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_date = models.DateField()
     isbn = models.CharField(max_length=13)
-    genre = models.CharField(max_length=50,default='n')
-    language = models.CharField(max_length=20,default='en')
-    cover_image = models.ImageField(upload_to='book_covers/',default=0)
+    genre = models.CharField(max_length=50, default="n")
+    language = models.CharField(max_length=20, default="en")
+    cover_image = models.ImageField(upload_to="book_covers/", default=0)
     pages = models.PositiveIntegerField(default=0)  # Default value set to 0
-    price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
