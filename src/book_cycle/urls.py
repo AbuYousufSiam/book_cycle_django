961f79f4fe420895167e4143ef81f9d73b07be03
ashrafuzzamanabir <lemplifeab@gmail.com>
2023-11-07 23:33:07 +0600

    update test

--- src/book_cycle/urls.py
+++ src/book_cycle/urls.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 # for media picture issue
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
-from ..books.views import update 
+
+
 
 # ........................................
+from books import views  # Import views from the current app
+from books.views import update
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("books.urls")),
+    path("update_server/", views.update, name="update"),
+]
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("books.urls")),path("update_server/", views.update, name="update"),]
 
 # for media picture loading issue
 if settings.DEBUG:
