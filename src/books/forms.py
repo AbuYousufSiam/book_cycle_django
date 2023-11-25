31bc1a342777bfd29a67780e16207421524fa891
ashrafuzzamanabir <lemplifeab@gmail.com>
2023-11-25 15:00:37 +0600

    test design

--- src/books/forms.py
+++ src/books/forms.py
@@ -14,14 +14,15 @@ class BookForm(forms.ModelForm):
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Required. Enter your first name.')
     last_name = forms.CharField(max_length=30, required=True, help_text='Required. Enter your last name.')
-
+    address = forms.CharField(max_length=255, required=False, help_text='Required. Your address.')
     class Meta:
         model = User
         fields = [
             'username', 
             'first_name', 
             'last_name', 
-            'email', 
+            'email',
+            'address', 
             'password1', 
             'password2', 
         ]
