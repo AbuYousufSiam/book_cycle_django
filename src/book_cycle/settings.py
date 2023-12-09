d5c93ca905ae07cab18d3b3f0d75fae585126222
ashrafuzzamanabir <lemplifeab@gmail.com>
2023-12-09 23:40:49 +0600

    testing new directory with mysql db

--- src/book_cycle/settings.py
+++ src/book_cycle/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "books",
     "widget_tweaks",
+    "books",
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,14 @@ WSGI_APPLICATION = "book_cycle.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "cloud",
+        "USER": "cloud",
+        # "USER": "root",
+        "PASSWORD": "cloud",
+        # "HOST": "127.0.0.1",
+        "HOST": "db",
+        "PORT": "3306",
     }
 }
 
--- src/books/models.py
+++ src/books/models.py
@@ -28,10 +28,10 @@ class Book(models.Model):
     details = models.CharField(max_length=500, default="No description")
     genre = models.CharField(max_length=50, default="Not defined")
     language = models.CharField(max_length=20, default="Not defined")
-    cover_image = models.ImageField(upload_to="book_covers/", default=0)
-    pages = models.PositiveIntegerField(default=0)  # Default value set to 0
+    cover_image = models.ImageField(upload_to="book_covers/", default="Not defined")
+    pages = models.PositiveIntegerField(default=1000)  # Default value set to 0
     is_available = models.CharField(max_length=20, choices=choice_list)
-    upload_date = models.DateField(auto_now_add=True)
+    upload_date = models.DateField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
