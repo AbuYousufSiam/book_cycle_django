6ec3a3d9aaa5e4541911e43fb7e4c9bfcbdd6c5c
ashrafuzzamanabir <lemplifeab@gmail.com>
2023-12-10 17:23:40 +0600

    env setup

--- src/book_cycle/settings.py
+++ src/book_cycle/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
+
+env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*f@jy+j$yi!8nxffm23twf26!ic66rl7eko-bzyl&2@68b@f5m"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 
 # Application definition
@@ -75,16 +77,29 @@ WSGI_APPLICATION = "book_cycle.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "cloud",
+#         "USER": "cloud",
+#         # "USER": "root",
+#         "PASSWORD": "cloud",
+#         # "HOST": "127.0.0.1",
+#         "HOST": "db",
+#         "PORT": "3306",
+#     }
+# }
+
 DATABASES = {
+    
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "cloud",
-        "USER": "cloud",
-        # "USER": "root",
-        "PASSWORD": "cloud",
-        # "HOST": "127.0.0.1",
-        "HOST": "db",
-        "PORT": "3306",
+         "ENGINE": env('ENGINE'),
+         "NAME": env('NAME'),
+         "USER": env('USER'),
+         "PASSWORD": env('PASSWORD'),
+         "HOST": env('HOST'),
+         "PORT": env('PORT'),
     }
 }
 
@@ -123,8 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = env('STATIC_URL')
+STATIC_ROOT = env('STATIC_ROOT')
+
 
 LOGIN_REDIRECT_URL = "/login/"
 LOGOUT_REDIRECT_URL = "/"
